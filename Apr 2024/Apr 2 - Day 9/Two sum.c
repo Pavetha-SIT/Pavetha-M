26b66094c5c609644c6ab58e2166e095ea78dd01
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-04-07 19:03:33 +0530

    Update Two sum.c

--- Apr 2024/Apr 2 - Day 9/Two sum.c
+++ Apr 2024/Apr 2 - Day 9/Two sum.c
@@ -1,16 +1,15 @@
-/*1.1. Two sum -Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+/*1. Two sum -Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 You can return the answer in any order.
-Example input: 
+input: 
  nums = [2,7,11,15], target = 9
 Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 */
 #include <stdio.h>
 
 int main()
 {
-    int a[1000];
+    int a[100];
     int n,m,b=0;
     scanf("%d",&n);
     printf("Enter elements:");
@@ -35,10 +34,9 @@ int main()
     }
     return 0;
 }
-/*Input:
+/*OUTPUT:
  4
  Enter elements:2,7,11,15
  9
- Output:
  [0,1]
 */
