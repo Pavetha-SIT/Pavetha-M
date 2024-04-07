a0b51f86f4145beaa473a5c6dc456dfee3fdc853
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-04-07 19:20:11 +0530

    Create Non repeated character.c

---
+++ Apr 2024/Apr 3 - Day 10/Non repeated character.c
@@ -0,0 +1,29 @@
+/*Program to print the non repeated character in the string  */
+
+Program:
+
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char p[100];
+    scanf("%s",&p);
+    int u[100]=0;
+    for(int i=0;i<strlen(p);i++)
+    {
+        u[p[i]]++;
+    }
+    for(i=0;i<strlen(p);i++)
+    {
+        if(u[p[i]]==1)
+        {
+            printf("%c ",p[i]);
+        }
+    }
+    return 0;
+}
+
+/* output:
+character
+h t e
+*/
