ad46b987cfa8e20ec1fe946d7c35bccf2ccedca8
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-04-19 22:06:19 +0530

    Update GCD of a number.c

--- Apr 2024/Apr 18 - Day 24/GCD of a number.c
+++ Apr 2024/Apr 18 - Day 24/GCD of a number.c
@@ -2,3 +2,22 @@
 
 program:
 
+#include<stdio.h>
+int gcd(int a,int b)
+{
+	if(a==0)
+		return b;
+	return gcd(b%a,a);
+}
+int main()
+{
+    int a,b;
+	  scanf("%d%d",&a,&b);
+	  rintf("%d",gcd(a,b));
+}
+
+/*
+output:
+45 60
+15
+  */
