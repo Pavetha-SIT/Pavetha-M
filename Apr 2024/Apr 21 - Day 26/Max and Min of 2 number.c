ee90b2400eb4e0996228b0a9e1a9fa1071db5efe
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-04-28 21:09:01 +0530

    Update Max and Min of 2 number.c

--- Apr 2024/Apr 21 - Day 26/Max and Min of 2 number.c
+++ Apr 2024/Apr 21 - Day 26/Max and Min of 2 number.c
@@ -1,3 +1,21 @@
 //Write a Program to find the Maximum and minimum of two numbers without using any loop or condition. 
 
 program:
+
+#include <stdio.h>
+int main() {
+    int num1, num2, max, min;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    max = num1 ^ ((num1 ^ num2) & -(num1 < num2));
+    min = num2 ^ ((num1 ^ num2) & -(num1 < num2));
+    printf("Maximum of %d and %d is: %d\n", num1, num2, max);
+    printf("Minimum of %d and %d is: %d\n", num1, num2, min);
+    return 0;
+}
+/*
+output:
+Enter two numbers: 5 9
+Maximum of 5 and 9 is: 9
+Minimum of 5 and 9 is: 5
+*/
