d2aa92f1c7f10dab1d2e491e47ff215c70cda11d
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-05-05 22:44:24 +0530

    Create count vowels in string.c

---
+++ May 2024/May 2 - Day 34/count vowels in string.c
@@ -0,0 +1,29 @@
+/*write a program to count number of vowels in a string*/
+
+#include <stdio.h>
+#include<ctype.h>
+int main()
+{
+    char s[50];
+    scanf("%s",s);
+    int i,count;
+    for(i=0;s[i]!='\0';i++) 
+    {
+        s[i]=tolower(s[i]); 
+    }
+    for(i=0;s[i]!='\0';i++)
+    {
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+        {
+            count++;
+        }
+        
+    }
+    printf("%d",count);
+}
+
+/*
+output
+string
+1
+*/
