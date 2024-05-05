44588e7eeb986641ee8714f2da5a0f4ba6fc74f2
Pavetha-SIT <159741882+Pavetha-SIT@users.noreply.github.com>
2024-05-05 22:28:55 +0530

    Create add even digit.c

---
+++ Apr 2024/Apr 29 - Day 31/add even digit.c
@@ -0,0 +1,17 @@
+// write a program to accept a number and add all the EVEN digit in that number*/
+
+#include<stdio.h>
+int main() 
+{
+    int num,a,sum = 0;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    while(num!=0) {
+        a=num%10;
+        if(a%2==0) { 
+            sum+=a; 
+        }
+        num/=10; 
+    }
+    printf("Sum of even digits in the number: %d\n", sum);
+}
